e5cfbe7ac7a943485f6aa7bb5cd4a5f1b97d76ee
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-08-17 15:42:08 -0300

    instituto controller

--- src/controllers/instituo_controller.js
+++ src/controllers/instituo_controller.js
@@ -1,24 +1,91 @@
 const models = require("../models");
 
-
-const getAllInstitutos = async (req,res) => {
-
+const getAllInstitutos = async (req, res) => {
+    models.instituto
+        .findAll(
+            {
+                offset: (parseInt(req.params.paginaActual) * parseInt(req.params.cantidad)),
+                limit: parseInt(req.params.cantidad),
+                attributes: ["id", "nombre","director"]
+            }
+        )
+      .then(instituto => res.send(instituto))
+      .catch(() => res.sendStatus(500));
 };
 
-const getOneInstitutos = async (req,res) => {
-    
+const findInstituto = (id, { onSuccess, onNotFound, onError }) => {
+    models.instituto
+      .findOne({
+        attributes: ["id", "nombre","director"],
+        where: { id }
+      })
+      .then(instituto => (instituto ? onSuccess(instituto) : onNotFound()))
+      .catch(() => onError());
+  };
+
+const getOneInstitutos = async (req, res) => {
+    findInstituto(req.params.id, 
+        {
+            onSuccess: instituto => res.send(instituto),
+            onNotFound: () => res.sendStatus(404),
+            onError: () => res.sendStatus(500)
+        }
+    );
 };
 
-const createdInstituto = async (req,res) => {
-    
+const createdInstituto = async (req, res) => {
+    models.instituto
+      .create({ nombre: req.body.nombre, director: req.body.director }) //Agregar atributos
+      .then(instituto => res.status(201).send({ id: instituto.id }))
+      .catch(error => 
+            {
+                if (error == "SequelizeUniqueConstraintError: Validation error") {
+                    res.status(400).send('Bad request: existe otro instituto con los mismos datos')
+                }
+                else {
+                    console.log(`Error al intentar insertar en la base de datos: ${error}`)
+                    res.sendStatus(500)
+                }
+            }     
+        );
 };
 
-const updateInstituto = async (req,res) => {
-    
+const updateInstituto = async (req, res) => {
+    const onSuccess = instituto =>
+        instituto
+            .update({ nombre: req.body.nombre, director: req.body.director }, { fields: ["nombre","director"] })
+            .then(() => res.sendStatus(200))
+            .catch(error => 
+                {
+                  if (error == "SequelizeUniqueConstraintError: Validation error") {
+                        res.status(400).send('Bad request: existe otro instituto los mismos datos')
+                    }
+                    else {
+                        console.log(`Error al intentar actualizar la base de datos: ${error}`)
+                        res.sendStatus(500)
+                    }
+                }
+            );
+      findInstituto(req.params.id, {
+      onSuccess,
+      onNotFound: () => res.sendStatus(404),
+      onError: () => res.sendStatus(500)
+    });
 };
 
-const deleteInstituto = async (req,res) => {
-    
+const deleteInstituto = async (req, res) => {
+    const onSuccess = instituto =>
+                instituto
+                    .destroy()
+                    .then(() => res.sendStatus(200))
+                    .catch(() => res.sendStatus(500));
+    findInstituto(req.params.id, 
+        {
+            onSuccess,
+            onNotFound: () => res.sendStatus(404),
+            onError: () => res.sendStatus(500)
+        }
+    );
 };
 
 module.exports = {
--- src/routes/instituto.js
+++ src/routes/instituto.js
@@ -1,88 +1,13 @@
-var express = require("express");
-var router = express.Router();
-var models = require("../models");
-var validador = require('../routes/validador');
+const express = require("express");
+const router = express.Router();
+const validador = require('../routes/validador');
+const institutosControllers = require('../controllers/instituo_controller.js');
 
-router.get("/:paginaActual&:cantidad",validador.validarToken, (req, res) => {
-    models.instituto
-      .findAll({
-        offset: (parseInt(req.params.paginaActual) * parseInt(req.params.cantidad)),
-        limit: parseInt(req.params.cantidad),
-        attributes: ["id", "nombre","director"]
-      })
-      .then(instituto => res.send(instituto))
-      .catch(() => res.sendStatus(500));
-  });
+router
+      .get("/:paginaActual&:cantidad", validador.validarToken, institutosControllers.getAllInstitutos)
+      .get("/:id", validador.validarToken, institutosControllers.getOneInstitutos)
+      .post("/", validador.validarToken, institutosControllers.createdInstituto)
+      .put("/:id", validador.validarToken, institutosControllers.updateInstituto)
+      .delete("/:id", validador.validarToken, institutosControllers.deleteInstituto)
   
-
-router.post("/",validador.validarToken, (req, res) => {
-    models.instituto
-      .create({ nombre: req.body.nombre, director: req.body.director }) //Agregar atributos
-      .then(instituto => res.status(201).send({ id: instituto.id }))
-      .catch(error => {
-        if (error == "SequelizeUniqueConstraintError: Validation error") {
-          res.status(400).send('Bad request: existe otro instituto con el mismo nombre')
-        }
-        else {
-          console.log(`Error al intentar insertar en la base de datos: ${error}`)
-          res.sendStatus(500)
-        }
-      });
-  });
-  
-  const findInstituto = (id, { onSuccess, onNotFound, onError }) => {
-    models.instituto
-      .findOne({
-        attributes: ["id", "nombre","director"],
-        where: { id }
-      })
-      .then(instituto => (instituto ? onSuccess(instituto) : onNotFound()))
-      .catch(() => onError());
-  };
-
-
-  router.get("/:id",validador.validarToken, (req, res) => {
-    findInstituto(req.params.id, {
-      onSuccess: instituto => res.send(instituto),
-      onNotFound: () => res.sendStatus(404),
-      onError: () => res.sendStatus(500)
-    });
-  });
-  
-
-  router.put("/:id",validador.validarToken, (req, res) => {
-    const onSuccess = instituto =>
-    instituto
-        .update({ nombre: req.body.nombre, director: req.body.director }, { fields: ["nombre","director"] })
-        .then(() => res.sendStatus(200))
-        .catch(error => {
-          if (error == "SequelizeUniqueConstraintError: Validation error") {
-            res.status(400).send('Bad request: existe otra instituto con el mismo nombre')
-          }
-          else {
-            console.log(`Error al intentar actualizar la base de datos: ${error}`)
-            res.sendStatus(500)
-          }
-        });
-      findInstituto(req.params.id, {
-      onSuccess,
-      onNotFound: () => res.sendStatus(404),
-      onError: () => res.sendStatus(500)
-    });
-  });
-
-  router.delete("/:id",validador.validarToken, (req, res) => {
-    const onSuccess = instituto =>
-    instituto
-        .destroy()
-        .then(() => res.sendStatus(200))
-        .catch(() => res.sendStatus(500));
-    findInstituto(req.params.id, {
-      onSuccess,
-      onNotFound: () => res.sendStatus(404),
-      onError: () => res.sendStatus(500)
-    });
-  });
-  
-  module.exports = router;
-  
+module.exports = router;
