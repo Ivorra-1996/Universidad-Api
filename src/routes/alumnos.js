7c3a4a648f0a01937c38d22320b0c4af1aafde0b
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-08-17 10:57:23 -0300

    Cambios

--- src/routes/alumnos.js
+++ src/routes/alumnos.js
@@ -1,23 +1,7 @@
 var express = require("express");
 var router = express.Router();
-
-var models = require("../models");
-
 var validador = require('../routes/validador');
 const alumnosControllers = require('../controllers/alumno_controller.js');
-
-
-const findalumnos = (id, { onSuccess, onNotFound, onError }) => {
-  models.alumnos
-    .findOne({
-      attributes: ["id","nombre","apellido","mail","dni","id_materia"],
-      where: { id }
-  })
-  .then(alumnos => (alumnos ? onSuccess(alumnos) : onNotFound()))
-  .catch(() => onError());
-};
-
-
 
 router
       .get("/:paginaActual&:cantidad", validador.validarToken, alumnosControllers.getAllAlumnos)
