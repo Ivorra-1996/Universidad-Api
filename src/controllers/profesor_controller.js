8d864954309ff7ecb3e35a94b63cb7f7877d2bac
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-08-17 16:10:46 -0300

    profesor controller

--- src/controllers/profesor_controller.js
+++ src/controllers/profesor_controller.js
@@ -1,28 +1,122 @@
 const models = require("../models");
 
-const getAllProfesores = async (req,res) => {
-
+const getAllProfesores = async (req, res) => {
+    models.profesor
+      .findAll(
+            {
+                offset: (parseInt(req.params.paginaActual) * parseInt(req.params.cantidad)),
+                limit: parseInt(req.params.cantidad),
+        
+                attributes: ["id","nombre","apellido","email", "id_materia"]
+            }
+        )
+      .then(profesor => res.send(profesor))
+      .catch(() => res.status(500).send("Error en el servidor"));
 };
 
-
-const getOneProfesor = async (req,res) => {
-
+const findProfesor = (id, { onSuccess, onNotFound, onError }) => {
+    models.profesor
+      .findOne(
+        {
+            attributes: ["id", "nombre","apellido","email","id_materia"],
+            /////////se agrega la asociacion 
+            where: { id }
+        }
+    )
+    .then(profesor => (profesor ? onSuccess(profesor) : onNotFound()))
+    .catch(() => onError());
 };
 
-
-const createProfesor = async (req,res) => {
-
+const getOneProfesor = async (req, res) => {
+    findProfesor(req.params.id, 
+                {
+                    onSuccess: profesor => res.send(profesor),
+                    onNotFound: () => res.status(404).send("Usuario no encontrado"),
+                    onError: () => res.status(500).send("Error en el servidor"),
+                }
+            );
 };
 
-
-const updateProfesor = async (req,res) => {
-
+const createProfesor = async (req, res) => {
+    models.profesor
+      .create(
+            {
+                nombre: req.body.nombre, 
+                apellido: req.body.apellido,
+                email: req.body.email,
+                id_materia: req.body.id_materia
+            }
+        ).then(profesor => res.status(201).send
+                (
+                    { 
+                        id: profesor.id,
+                        nombre: profesor.nombre,
+                        apellido: profesor.apellido,
+                        email: profesor.email,
+                        id_materia: profesor.id_materia,
+                    }
+                )
+            )
+      .catch(error => 
+            {
+                if (error == "SequelizeUniqueConstraintError: Validation error") {
+                    res.status(406).send('Ya existe un usuario con ese correo electronico')
+                }
+                else {
+                    console.log(`Error al intentar insertar en la base de datos: ${error}`)
+                    res.sendStatus(500)
+                }
+            }
+        );
 };
 
+const updateProfesor = async (req, res) => {
+    const onSuccess = profesor =>
+        profesor
+        .update(
+                { 
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    email: req.body.email,
+                    id_materia: req.body.id_materia
+                },
+                { 
+                    fields: ["nombre","apellido","email","id_materia"] 
+                }
+            )
+        .then(() => res.sendStatus(200))
+        .catch(error => 
+            {
+                if (error == "SequelizeUniqueConstraintError: Validation error") {
+                    res.status(400).send('Bad request: existe otro usuario con el mismo email!!!!')
+                }
+                else {
+                    res.sendStatus(500).send(`Error al intentar actualizar la base de datos: ${error}`)
+                }
+            }
+        );
+        findProfesor(req.params.id, 
+            {
+                onSuccess,
+                onNotFound: () => res.sendStatus(404),
+                onError: () => res.sendStatus(500)
+            }
+        );
+};
 
-
-const deleteProfesor = async (req,res) => {
-
+const deleteProfesor = async  (req, res) => {
+    const onSuccess = profesor =>
+      profesor
+        .destroy()
+        .then(() => res.sendStatus(200))
+        .catch(() => res.sendStatus(500));
+    findProfesor(req.params.id, 
+        {
+            onSuccess,
+            onNotFound: () => res.sendStatus(404),
+            onError: () => res.sendStatus(500)
+        }
+    );
 };
 
 module.exports = {
--- src/routes/profesor.js
+++ src/routes/profesor.js
@@ -1,101 +1,14 @@
-var express = require("express");
-var router = express.Router();
-var models = require("../models");
-var validador = require('../routes/validador');
+const express = require("express");
+const router = express.Router();
+const validador = require('../routes/validador');
+const profesorController = require('../controllers/profesor_controller.js');
 
-router.get("/:paginaActual&:cantidad",validador.validarToken, (req, res) => {
-  models.profesor
-    .findAll({
-      offset: (parseInt(req.params.paginaActual) * parseInt(req.params.cantidad)),
-      limit: parseInt(req.params.cantidad),
-
-      attributes: ["id","nombre","apellido","email", "id_materia"]
-    })
-    .then(profesor => res.send(profesor))
-    .catch(() => res.status(500).send("Error en el servidor"));
-});
-
-router.post("/",validador.validarToken, (req, res) => {
-    models.profesor
-      .create({
-        nombre: req.body.nombre, 
-        apellido: req.body.apellido,
-        email: req.body.email,
-        id_materia: req.body.id_materia
-      }).then(profesor => res.status(201).send({ 
-        id: profesor.id,
-        nombre: profesor.nombre,
-        apellido: profesor.apellido,
-        email: profesor.email,
-        id_materia: profesor.id_materia,
-      }))
-      .catch(error => {
-        if (error == "SequelizeUniqueConstraintError: Validation error") {
-          res.status(406).send('Ya existe una cuenta con esos datos')
-        }
-        else {
-          console.log(`Error al intentar insertar en la base de datos: ${error}`)
-          res.sendStatus(500)
-        }
-      });
-  });
-
-const findProfesor = (id, { onSuccess, onNotFound, onError }) => {
-  models.profesor
-    .findOne({
-      attributes: ["id", "nombre","apellido","email","id_materia"],
-      /////////se agrega la asociacion 
- 
-      ////////////////////////////////
-      where: { id }
-    })
-    .then(profesor => (profesor ? onSuccess(profesor) : onNotFound()))
-    .catch(() => onError());
-};
-
-router.get("/:id",validador.validarToken, (req, res) => {
-  findProfesor(req.params.id, {
-    onSuccess: profesor => res.send(profesor),
-    onNotFound: () => res.status(404).send("Usuario no encontrado"),
-    onError: () => res.status(500).send("Error en el servidor"),
-  });
-});
-
-//funciona 
-router.put("/:id",validador.validarToken, (req, res) => {
-  const onSuccess = profesor =>
-      profesor
-      .update({ nombre: req.body.nombre,apellido: req.body.apellido,email: req.body.email,id_materia: req.body.id_materia},
-           { fields: ["nombre","apellido","email","id_materia"] })
-      .then(() => res.sendStatus(200))
-      .catch(error => {
-        if (error == "SequelizeUniqueConstraintError: Validation error") {
-          res.status(400).send('Bad request: existe otro usuario con el mismo email!!!!')
-        }
-        else {
-          res.sendStatus(500).send(`Error al intentar actualizar la base de datos: ${error}`)
-        }
-      });
-      findProfesor(req.params.id, {
-    onSuccess,
-    onNotFound: () => res.sendStatus(404),
-    onError: () => res.sendStatus(500)
-  });
-});
-
-router.delete("/:id",validador.validarToken, (req, res) => {
-  const onSuccess = profesor =>
-    profesor
-      .destroy()
-      .then(() => res.sendStatus(200))
-      .catch(() => res.sendStatus(500));
-  findProfesor(req.params.id, {
-    onSuccess,
-    onNotFound: () => res.sendStatus(404),
-    onError: () => res.sendStatus(500)
-  });
-});
-
-
+router
+      .get("/:paginaActual&:cantidad", validador.validarToken, profesorController.getAllProfesores)
+      .get("/:id", validador.validarToken, profesorController.getOneProfesor)
+      .post("/", validador.validarToken, profesorController.createProfesor)
+      .put("/:id", validador.validarToken, profesorController.updateProfesor)
+      .delete("/:id", validador.validarToken, profesorController.deleteProfesor)
+      
 module.exports = router;
 
