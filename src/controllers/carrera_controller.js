971dce6133f8e01e24252baea828b2d02668bee7
Ivorra-1996 <ivorrajosemaria@gmail.com>
2023-08-17 11:55:20 -0300

    carrera controllers

--- src/controllers/carrera_controller.js
+++ src/controllers/carrera_controller.js
@@ -1,1 +1,24 @@
 const models = require("../models");
+
+const getAllCarreras = async (req,res) => {
+
+};
+
+const getOneCarrera = async (req,res) => {
+
+};
+
+const createdCarrera = async (req,res) => {
+
+};
+
+const updateCarrera = async (req,res) => {
+
+};
+
+module.exports = {
+    getAllCarreras,
+    getOneCarrera,
+    createdCarrera,
+    updateCarrera
+};
